f1db88fdf806f8a1e37788ebc064a0bfda194f2b
suguru <u.sgr.22114w@gmail.com>
2017-04-12 11:12:33 +0900

    remove matplot

--- gpioController.py
+++ gpioController.py
@@ -6,7 +6,7 @@ class GPIO(object):
         self.pin1 = 11 #p0
         self.pin2 = 12 #p5
         self.pin3 = 15 #p3
-        self.pin3 = 13 #p4
+        self.pin4 = 13 #p4
         self.initGPIO()
 
     def initGPIO(self):
