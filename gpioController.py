4a0a372dc633e14f74538857c82b1244cba609e4
suguru <u.sgr.22114w@gmail.com>
2017-04-12 11:39:49 +0900

    gpio setup fixed

--- gpioController.py
+++ gpioController.py
@@ -29,12 +29,12 @@ class GPIO(object):
     def GoStraight(self,time):
         status = True
         while status:
-            self.GPIO.output(PIN1, True)
-            self.GPIO.output(PIN3, True)
+            self.GPIO.output(self.pin1, True)
+            self.GPIO.output(self.pin4, True)
             time.sleep(time)
             status = False
-        self.GPIO.output(PIN1, False)
-        self.GPIO.output(PIN3, False)
+        self.GPIO.output(self.pin1, False)
+        self.GPIO.output(self.pin4, False)
         return
 
     def TurnAround(self,rotate_time,rotate_way):
@@ -42,13 +42,13 @@ class GPIO(object):
             status = True
             while status:
                 GPIO = self.GPIO
-                GPIO.output(PIN2, True)
-                GPIO.output(PIN3, True)
+                GPIO.output(self.pin2, True)
+                GPIO.output(self.pin3, True)
                 time.sleep(time)
                 status = False
             GPIO = self.GPIO
-            GPIO.output(PIN2, False)
-            GPIO.output(PIN3, False)
+            GPIO.output(self.pin2, False)
+            GPIO.output(self.pin3, False)
         else:
             status = True
             while status:
